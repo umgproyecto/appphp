ea08d2d6a311e30576efe3e291e86f3fb2a75747
Carlos Vasquez <carls10vasquez@gmail.com>
2021-05-09 18:19:40 -0600

    app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,15 +2,14 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1">-->
+<!----><!-- CSRF Token -->
+<!--    <meta name="csrf-token" content="{{ csrf_token() }}">-->
     <title>panel de administración</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+<!--    <link rel="dns-prefetch" href="//fonts.gstatic.com">-->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
